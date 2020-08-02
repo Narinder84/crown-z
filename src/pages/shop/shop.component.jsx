beb9f68849824c0a42dd4560fb0f9154e964154a
getdev84 <getdev84@gmail.com>
2020-08-02 01:12:52 -0400

    update

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,7 +9,7 @@ const ShopPage = ({match})=> {
     
     return (
       <Switch>
-        <Scroll height = {`600px`} >
+        <Scroll height = {`550px`} >
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route exact path={`${match.path}/:collectionId`} component={Collections} />
         </Scroll>
