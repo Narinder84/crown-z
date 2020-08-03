e1049fc694286719b16fa564cb0261a3e7d4aa43
getdev84 <getdev84@gmail.com>
2020-08-02 20:36:56 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{lazy} from 'react';
+import React,{lazy,Suspense} from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 
@@ -7,19 +7,21 @@ import   './App.css';
 // import {HomePage , ShopPage , SignInAndSignUpPage , CartTable } from './pages';
 
 import {Header} from './components';
-const HomePage = lazy('./pages');
-const ShopPage =lazy('./pages');
-const SignInAndSignUpPage=lazy('./pages');
-const CartTable=lazy('./pages');
+const HomePage = lazy(()=> import('./pages/homepage/homepage.component.jsx'));
+const ShopPage = lazy(()=> import('./pages/shop/shop.component.jsx'));
+const SignInAndSignUpPage =lazy(()=> import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'));
+const CartTable=lazy(()=> import('./pages/crat-table/cart-table.component.jsx'));
 function App() {
   return (
     <div>
       <Header />
       <Switch>
+        <Suspense fallback={<div>...Loading</div>} >
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
         <Route path='/signin' component={SignInAndSignUpPage} />
         <Route path='/cart_table' component={CartTable} />
+        </Suspense>
       </Switch>
     </div>
   );
