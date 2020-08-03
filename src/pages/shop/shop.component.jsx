4b550827b0eb0387ed9eb44514f4929d6a8613e6
getdev84 <getdev84@gmail.com>
2020-08-02 21:05:09 -0400

    update

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React,{lazy,Suspense} from 'react';
 import {Route,Switch} from 'react-router-dom'
 import  Scroll from '../../components/scroll/scroll.component'
-import {CollectionsOverview} from '../../components'
-import {Collections} from '../../components'
+// import {CollectionsOverview} from '../../components'
+// import {Collections} from '../../components'
+
+const CollectionsOverview = lazy(()=>import('../../components/collectionsOverview/collectionsOverview.jsx'));
+const Collections=lazy(()=> import('../../components/collections/collections.component.jsx'));
 
 const ShopPage = ({match})=> {
 
@@ -10,8 +13,10 @@ const ShopPage = ({match})=> {
     return (
       <Switch>
         <Scroll height = {`520px`} >
+        <Suspense fallback = {<div>....Loading</div>} >
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route exact path={`${match.path}/:collectionId`} component={Collections} />
+        </Suspense>
         </Scroll>
         
       </Switch>
