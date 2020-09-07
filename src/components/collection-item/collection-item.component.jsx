a79fd87b315b5b9302b4dd85fd80016af03796b0
getdev84 <getdev84@gmail.com>
2020-09-06 20:04:11 -0400

    ok

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import {connect} from 'react-redux'
+/** @format */
 
-import CustomeButton from '../custom-button/custom-button.component'
-import {addItemToCart} from '../../redux/cart/cart.ations'
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import { addItemToCart } from '../../redux/cart/cart.ations';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({item,addItemToCart}) =>{
-  
-   const {  name, price, imageUrl} = item
-  return  (
-    <div className='collection-item'>
-      <div
-        className='image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-        
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <span className='bt' ><CustomeButton onClick ={()=>addItemToCart( item)} >Add to Cart</CustomeButton></span>
-    </div>
-  );
-}
+const CollectionItem = ({ item, addItemToCart }) => {
+	const { name, price, imageUrl } = item;
+	return (
+		<div className='collection-item'>
+			<div
+				className='image'
+				style={{
+					backgroundImage: `url(${imageUrl})`,
+				}}
+			/>
 
-const mapDispatchToProps = (dispatch)=>({
-  addItemToCart : (item)=>dispatch(addItemToCart(item)) 
-})
+			<div className='collection-footer'>
+				<span className='name'>{name}</span>
+				<span className='price'>{price}</span>
+			</div>
+			<span className='bt'>
+				<CustomButton onClick={() => addItemToCart(item)}>
+					Add to Cart
+				</CustomButton>
+			</span>
+		</div>
+	);
+};
 
-export default  connect(null,mapDispatchToProps)( CollectionItem);
+const mapDispatchToProps = (dispatch) => ({
+	addItemToCart: (item) => dispatch(addItemToCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,51 +1,58 @@
+/** @format */
+
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {connect } from 'react-redux'
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import CartIcon from '../cart-icon/cart-icon.component'
-import style from  './header.module.scss';
-import CartDropDown from '../cart-dropdown/cart-dropdown.js'
+import CartIcon from '../cart-icon/cart-icon.component';
+import style from './header.module.scss';
+import CartDropDown from '../cart-dropdown/cart-dropdown.js';
 
+const Header = (props) => {
+	const { isHidden } = props;
 
-const Header = (props) =>  {
-const {isHidden} = props
+	return (
+		<div>
+			<div className={style.header}>
+				<div className={style.left_side}>
+					<Link className={style.logoContainer} to='/'>
+						<Logo className={style.logo} />
+						<span className='ltd'>Crown Cloths Ltd.</span>
+						<p className='cr'>Crown Ltd.</p>
+					</Link>
+				</div>
 
-return (
-  <div>
-    <div className={style.header}>
-      <div className={style.left_side} >
-        <Link className={style.logoContainer} to='/'>
-      
-          <Logo className={style.logo} />
-          <span className='ltd' >Crown Cloths Ltd.</span>
-          <p className='cr' >Crown Ltd.</p>  
-        </Link>
-    
-      </div>
-    
-      <div className={style.right_side}>
-        <Link className={style.options_all} to='/shop'>Shop</Link>
-        <Link className={style.options} to='/signin'>Log-In</Link>
-        <Link className={style.options_mobile} to='/signin_mobile'>Sign-In</Link>
-        <Link className={style.options_mobile} to='/register_mobile'>Register</Link>
-        <Link className={style.options_all} to='/cart_table'>Check-Out</Link>
-        <Link className={style.options_all} >
-          <CartIcon/>
-        
-        </Link>
-      
-      </div>
-    
-      </div>
-        {isHidden ? (null):(<div className={style.cartDropDown} >
-        <CartDropDown/>
-      </div>)}
-  </div>
-);
-
-}
-const mapStateToProps=(state)=>({
-  isHidden: state.cart.ishidden
-})
+				<div className={style.right_side}>
+					<Link className={style.options_all} to='/shop'>
+						Shop
+					</Link>
+					<Link className={style.options} to='/signin'>
+						Log-In
+					</Link>
+					<Link className={style.options_mobile} to='/signin_mobile'>
+						Sign-In
+					</Link>
+					<Link className={style.options_mobile} to='/register_mobile'>
+						Register
+					</Link>
+					<Link className={style.options_all} to='/cart_table'>
+						Check-Out
+					</Link>
+					<Link className={style.options_all}>
+						<CartIcon />
+					</Link>
+				</div>
+			</div>
+			{isHidden ? null : (
+				<div className={style.cartDropDown}>
+					<CartDropDown />
+				</div>
+			)}
+		</div>
+	);
+};
+const mapStateToProps = (state) => ({
+	isHidden: state.cart.ishidden,
+});
 
 export default connect(mapStateToProps)(Header);
